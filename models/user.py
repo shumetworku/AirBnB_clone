cbee9bca506bce530665e9c7c93cddbd0b1d3ba6
calebonyedikachi <117799243+calebonyedikachi@users.noreply.github.com>
2023-05-14 02:23:17 +0100

    User Class

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class for managing user objects"""
+    """For Class for managing user objects"""
 
     email = ""
     password = ""
