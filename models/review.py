e440b19cbd2c9cad7754422fd4b9210c4060cce8
calebonyedikachi <117799243+calebonyedikachi@users.noreply.github.com>
2023-05-14 02:21:31 +0100

    Class for review object

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class for managing review objects"""
+    """For class for managing review objects"""
 
     place_id = ""
     user_id = ""
